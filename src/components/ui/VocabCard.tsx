83621bb894e8c1d21ad482565461e8c3870d928a
sebastianod <sebastianochoad@gmail.com>
2024-06-09 21:09:28 +0200

    Add show-hide buttons to vocabCards

--- src/components/ui/VocabCard.tsx
+++ src/components/ui/VocabCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Card,
   CardImage,
@@ -9,7 +10,7 @@ import {
   CardSubTitle,
   CardFooter,
 } from "./card";
-import { Pencil, Trash2 } from "lucide-react";
+import { Pencil, Trash2, ChevronDown, ChevronUp } from "lucide-react";
 
 export interface VocabItem {
   vocab_id: number;
@@ -26,23 +27,41 @@ interface VocabCardProps {
 }
 
 const VocabCard: React.FC<VocabCardProps> = ({ word }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <Card className="h-fit min-w-72 max-w-96 transition duration-300 ease-out hover:scale-[1.02] hover:shadow-slate-700">
       <CardImage src={word.vocab_image_url} alt={word.vocab_word} />
       <CardHeader>
         <CardTitle>{word.vocab_word}</CardTitle>
-        <CardDescription>{word.vocab_definition}</CardDescription>
+        <div className="flex justify-center">
+          {isExpanded ? (
+            <ChevronUp className="cursor-pointer" onClick={toggleExpansion} />
+          ) : (
+            <ChevronDown className="cursor-pointer" onClick={toggleExpansion} />
+          )}
+        </div>
       </CardHeader>
-      <CardContent className="flex flex-col gap-2">
-        <CardSubTitle>Context</CardSubTitle>
-        <p>{word.vocab_context}</p>
-        <CardSubTitle>Example</CardSubTitle>
-        <p>{word.vocab_example}</p>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Pencil className="cursor-pointer" />
-        <Trash2 className="cursor-pointer" />
-      </CardFooter>
+      <div
+        className={`overflow-hidden transition-all duration-500 ${
+          isExpanded ? "max-h-[1000px] opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <CardContent className="flex flex-col gap-2">
+          <CardDescription>{word.vocab_definition}</CardDescription>
+          <CardSubTitle>Context</CardSubTitle>
+          <p>{word.vocab_context}</p>
+          <CardSubTitle>Example</CardSubTitle>
+          <p>{word.vocab_example}</p>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Pencil className="cursor-pointer" />
+          <Trash2 className="cursor-pointer" />
+        </CardFooter>
+      </div>
     </Card>
   );
 };
